c94187d8072762164e92ec4a2d8519c319ee6ba2
libn <libn@shanghaitech.edu.cn>
2023-05-19 14:57:01 +0800

    fix bug in MSE color mean loss

--- utils.py
+++ utils.py
@@ -64,13 +64,12 @@ def p_losses(denoise_model, img, R, T, K, logsnr, noise=None, loss_type="l2", co
     else:
         raise NotImplementedError()
 
-    rec_img = reconstruct_z_start(z_noisy, predicted_noise, logsnr)
-
+    rec_img = reconstruct_z_start(z_noisy, predicted_noise, logsnr.to(dev()))
     img_color_mean = torch.mean(z, dim=(2, 3))
-    rec_color_mean = torch.mean(rec_img, dim=(2, 3))
+    rec_color_mean = torch.mean(rec_img, dim=(2, 3)).to(dev())
 
     color_loss = torch.nn.MSELoss()
-    color_loss = (logsnr + 20) / 20 * color_loss(img_color_mean, rec_color_mean)
+    color_loss = (logsnr.to(dev()) + 20) / 20 * color_loss(img_color_mean, rec_color_mean)
 
     return loss + color_loss
 
